bba74ace8f3c6c7f217046af6ea1a7196df26b24
kumaran2328 <90097602+kumaran2328@users.noreply.github.com>
2021-09-04 18:44:12 +0530

    Create 2. eg of object and class.java

---
+++ 2. eg of object and class.java
@@ -0,0 +1,10 @@
+public	class Student1{  
+	 int id;
+	 String name;
+
+    public static void main(String args[]) {
+     Student1 s1=new Student1();//creating an object of Student  
+      System.out.println(s1.id);  
+      System.out.println(s1.name);  
+    }
+}
