7b93c1f7ec51c70c84f0d1e92f2f0d7bc0235696
kumaran2328 <90097602+kumaran2328@users.noreply.github.com>
2021-09-16 23:26:26 +0530

    Create Program of counter without static variable.java

---
+++ Program of counter without static variable.java
@@ -0,0 +1,12 @@
+public class Counter{  
+	int count=0;//will get memory when instance is created  
+	Counter(){  
+	count++;  
+	System.out.println(count);  
+	}  
+	public static void main(String args[]){  
+	Counter c1=new Counter();  
+	Counter c2=new Counter();  
+	Counter c3=new Counter();  
+	}  
+}  
