2b84fe26250b98292725ae22bc23ccbef8cdf012
kumaran2328 <90097602+kumaran2328@users.noreply.github.com>
2021-09-17 16:11:56 +0530

    Create 36 prg.java

---
+++ 36 prg.java
@@ -0,0 +1,7 @@
+public class TestSubstring{  
+	 public static void main(String args[]){  
+	String s="Sachin";  
+	 System.out.println(s.startsWith("Sa"));//true  
+	 System.out.println(s.endsWith("n"));//true  
+  }
+}
