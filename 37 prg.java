c6d58b118ac51b6f7275f2f58e281ec88dffa21e
kumaran2328 <90097602+kumaran2328@users.noreply.github.com>
2021-09-17 16:12:52 +0530

    Create 37 prg.java

---
+++ 37 prg.java
@@ -0,0 +1,7 @@
+public class TestSubstring{  
+	 public static void main(String args[]){  
+	String s="Sachin";  
+	System.out.println(s.charAt(0));//S  
+	System.out.println(s.charAt(3));//h
+   }
+}
