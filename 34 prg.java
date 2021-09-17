1b8b980ea955abddc38c20cb91d9608e3a952ada
kumaran2328 <90097602+kumaran2328@users.noreply.github.com>
2021-09-17 15:51:27 +0530

    Create 34 prg.java

---
+++ 34 prg.java
@@ -0,0 +1,8 @@
+public class TestSubstring{  
+	 public static void main(String args[]){  
+	String s="Sachin";  
+	System.out.println(s.toUpperCase());//SACHIN  
+	System.out.println(s.toLowerCase());//sachin  
+    System.out.println(s);
+}
+}
