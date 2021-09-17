7d9403074d3ea2cd0772131a9fcb293e31f4a03a
kumaran2328 <90097602+kumaran2328@users.noreply.github.com>
2021-09-17 15:28:20 +0530

    Create 17. Program of counter by static variable.java

---
+++ 17. Program of counter by static variable.java
@@ -0,0 +1,13 @@
+public class Counter2{  
+	static int count=0;//will get memory only once and retain its value  
+	Counter2(){  
+	count++;  
+	System.out.println(count);  
+	}  
+	public static void main(String args[]){  
+	Counter2 c1=new Counter2();  
+	Counter2 c2=new Counter2();  
+	Counter2 c3=new Counter2();  
+	  
+	 }  
+}  
